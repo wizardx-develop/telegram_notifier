c272b12b00ab0e87aa68fc3443293352a26f76fb
wizardx-developer <wizardxdeveloper@gmail.com>
2025-12-09 08:50:59 +0300

    add logic for get commit message

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
+	"strings"
 
+	"wizardx/telegram_notifier/internal/config"
 	"wizardx/telegram_notifier/internal/services/forgejo"
 	"wizardx/telegram_notifier/internal/services/telegram"
+
+	"code.gitea.io/sdk/gitea"
 )
 
 // LoggerInit настройка логирования
@@ -21,20 +26,39 @@ func LoggerInit() {
 func main() {
 	LoggerInit()
 
-	pr := &forgejo.PullRequestAction{}
+	pr := &forgejo.ActionPayload{}
 
 	err := pr.Parse()
 	if err != nil {
 		os.Exit(1)
 	}
-	msg, err := telegram.CreateMessage(pr)
+
+	separator := strings.Repeat("*", 40)
+
+	fmt.Println(separator)
+	client, err := forgejo.CreateClient(
+		os.Getenv(config.ForgejoServerURL.ToString()),
+		os.Getenv(config.ForgejoToken.ToString()),
+		[]gitea.ClientOption{},
+	)
 	if err != nil {
+		slog.Error("create client error", "error", err)
+		os.Exit(1)
+	}
+	commitMsg, err := forgejo.GetCommitMsg(client, pr)
+	if err != nil {
+		slog.Error("get commit msg error", "error", err)
+		os.Exit(1)
+	}
+	msg, err := telegram.CreateMessage(pr, commitMsg)
+	if err != nil {
+		slog.Error("create message failed", "error", err)
 		os.Exit(1)
 	}
 
 	err = telegram.SendMessage(msg)
 	if err != nil {
-		slog.Error("failed send telegram message", "error", err)
+		slog.Error("send telegram message failed", "error", err)
 		os.Exit(1)
 	}
 }
